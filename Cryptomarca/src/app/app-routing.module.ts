f527cbf23c48e2b6358a7a39b673f26ba210adce
Florencia - Ubuntu 20.04 <ing.florencia.bergesio@gmail.com>
2022-01-17 21:21:47 -0300

    Se crean redirecciones a la pagina de Inicio y al componente de pagina no encontrada

--- Cryptomarca/src/app/app-routing.module.ts
+++ Cryptomarca/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 
 const routes: Routes = [
@@ -21,7 +22,21 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
-  },  
+  },
+  {
+    path: '',
+    redirectTo: '/inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    redirectTo: '/inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
 ];
 
 @NgModule({
