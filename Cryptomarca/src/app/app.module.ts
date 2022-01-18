2550c2e17c9bc1f4f94454c0ae3b6440eeec123c
wiise2 <pablogeo1995@gmail.com>
2022-01-18 17:23:00 -0300

    Navbar Responsive
    Ya está de nuevo el navbar responsive integrado con bootstrap, lo dejo en color negro hasta ver que va a pasar con el hero para ahí acomodarlo segun los colores que tenga el hero.

--- Cryptomarca/src/app/app.module.ts
+++ Cryptomarca/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,7 @@ import { PagesModule } from './pages/pages.module';
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
-    PagesModule
+    PagesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
