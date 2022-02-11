08088ca10f6517c57132b7956d38ad1c9fb5dad0
Guillermo Velasco <guillermo.a.velasco@gmail.com>
2022-02-11 11:12:26 -0300

    Arreglando Referencias del Proyecto a Crypto

--- crypto/src/app/app.component.spec.ts
+++ crypto/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'catacoin'`, () => {
+  it(`should have as title 'cryptomarca'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('catacoin');
+    expect(app.title).toEqual('cryptomarca');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('catacoin app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cryptomarca app is running!');
   });
 });
