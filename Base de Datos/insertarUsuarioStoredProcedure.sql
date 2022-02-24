354ee9671f7bdb2daf4b562bb82acfedca50917c
FlorBergesio <ing.florencia.bergesio@gmail.com>
2022-02-23 21:20:53 -0300

    Se agregan stored procedures para listar los usuarios e insertar usuario en la base de datos

---
+++ Base de Datos/insertarUsuarioStoredProcedure.sql
@@ -0,0 +1,41 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Método de insertar un Usuario
+-- =============================================
+CREATE PROCEDURE insertarUsuario 
+	-- Add the parameters for the stored procedure here
+	@nombre varchar(50),
+	@email varchar(50),
+	@contraseña varchar(25),
+	@dni varchar(12),
+	@fk_provincia bigint,
+	@fk_banco bigint,
+	@cbu varchar(20),
+	@fechaNacimiento dateTime
+AS
+BEGIN
+	INSERT INTO [dbo].[usuarios]
+           ([nombre]
+           ,[email]
+           ,[contraseña]
+           ,[dni]
+           ,[fk_provincia]
+           ,[fk_banco]
+           ,[cbu]
+           ,[fechaNacimiento])
+     VALUES
+           (
+			   @nombre,
+			   @email,
+			   @contraseña,
+			   @dni,
+			   @fk_provincia,
+			   @fk_banco,
+			   @cbu,
+			   @fechaNacimiento
+           )
+END
+GO
