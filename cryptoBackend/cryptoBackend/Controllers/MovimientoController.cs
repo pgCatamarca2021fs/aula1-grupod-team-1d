c3f79a194793e58e4039ce56cef22bb04cb50cf5
FlorBergesio <ing.florencia.bergesio@gmail.com>
2022-03-10 21:16:43 -0300

    Se crea el controlador de Movimiento

---
+++ cryptoBackend/cryptoBackend/Controllers/MovimientoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using cryptoBackend.Models;
+
+
+namespace cryptoBackend.Controllers
+{
+    public class MovimientoController : ApiController
+    {
+        // GET: api/Movimiento
+        public IEnumerable<Movimiento> Get()
+        {
+            GestorMovimiento gestor = new GestorMovimiento();
+
+            return gestor.ListarMovimientosDeUsuario(2);
+        }
+
+        // GET: api/Movimiento/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Movimiento
+        public void Post([FromBody] Movimiento nuevoMovimiento)
+        {
+            GestorMovimiento gestor = new GestorMovimiento();
+            gestor.CrearMovimiento(nuevoMovimiento);
+        }
+
+        // PUT: api/Movimiento/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Movimiento/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
