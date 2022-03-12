e68bc642190c7c61d40394b4bfd8dab88f6d237b
FlorBergesio <ing.florencia.bergesio@gmail.com>
2022-03-12 11:43:15 -0300

    La informacion de movimiento se muestra de manera mas clara

--- crypto/src/app/forms/sections/operations/operations.component.ts
+++ crypto/src/app/forms/sections/operations/operations.component.ts
@@ -1,5 +1,32 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { OperationsService } from 'src/app/services/operations.service';
+import { WalletService } from 'src/app/services/wallet.service';
+import { MovementTypeService } from 'src/app/services/movement-type.service';
+import { MoneyService } from 'src/app/services/money.service';
+
+import { map } from 'rxjs/operators';
+
+interface Operation {
+  id:number;
+  fk_billeteraMoneda_Origen:number;
+  fk_billeteraMoneda_Destino:number;
+  cantidad_Origen:number;
+  cantidad_Destino:number;
+  fecha:string;
+  fk_tipoMovimiento:number;
+}
+
+interface Wallet
+{precio:number,moneda:string, idBilletera:number, idMoneda:number}
+
+interface Money
+{
+  id:number;
+  nombre:string;
+  cotizacion:number;
+  fecha_cotizacion:string;
+  direccion_contrato:string;
+}
 
 @Component({
   selector: 'app-operations',
@@ -9,12 +36,68 @@ import { OperationsService } from 'src/app/services/operations.service';
 export class OperationsComponent implements OnInit {
   idUsuario:number=2; //hacerlo dinamico
   operations:any; 
+  operationsReadable: any;
+  wallets: any;
+  movementTypes: any;
+  coins: any;
 
-  constructor(private operationsServ:OperationsService) {this.operations=[];  }
+  constructor(private operationsServ:OperationsService, private walletServ:WalletService,  private typeServ:MovementTypeService, private moneyServ:MoneyService) {
+    this.operations=[]; 
+    this.operationsReadable=[]; 
+    this.wallets=[];
+    this.movementTypes=[];
+    this.coins=[];
+  }
+
+  getMovementType(id:number){
+    console.log(id)
+    
+    return id;
+      
+    
+  }
 
   ngOnInit(): void {
-    this.operationsServ.get(this.idUsuario).subscribe(data=>{ console.log(data); this.operations=data; });
-    console.log(this.operations);    
+    this.typeServ.list().subscribe(data=>{ 
+      //console.log("movementTypes",data); 
+      this.movementTypes=data; 
+    });  
+
+    this.walletServ.get(this.idUsuario).subscribe(data=>{ 
+      console.log("wallets",data); 
+      this.wallets=data; 
+    });
+
+    this.moneyServ.list().subscribe(data=>{ 
+      console.log("coins",data); 
+      this.coins=data; 
+    });
+
+    this.operationsServ.get(this.idUsuario).subscribe(data=>{ 
+      //console.log("operations",data); 
+      this.getMovementType(1);
+      this.operations=data; 
+
+      this.operationsReadable=this.operations.map((operation: Operation) => {
+        let billetera_Origen = this.wallets.find((wallet: Wallet) => wallet.idBilletera == operation.fk_billeteraMoneda_Origen);
+        let billetera_Destino = this.wallets.find((wallet: Wallet) => wallet.idBilletera == operation.fk_billeteraMoneda_Destino);
+        return {
+          id: operation.id, 
+          fk_billeteraMoneda_Origen: operation.fk_billeteraMoneda_Origen,
+          billetera_Origen: billetera_Origen.idBilletera, 
+          fk_billeteraMoneda_Destino: operation.fk_billeteraMoneda_Destino, 
+          billetera_Destino: billetera_Destino.idBilletera, 
+          cantidad_Origen: operation.cantidad_Origen,
+          moneda_Origen: this.coins.find((coin: Money) => coin.id == billetera_Origen.idMoneda), 
+          cantidad_Destino: operation.cantidad_Destino,
+          moneda_Destino: this.coins.find((coin: Money) => coin.id == billetera_Destino.idMoneda), 
+          fecha: operation.fecha, 
+          tipoMovimiento: this.movementTypes[operation.fk_tipoMovimiento - 1].descripcion, 
+        };
+      });
+
+      console.log("operationsReadable",this.operationsReadable); 
+    }); 
   }
 
 }
