3b7ab69c4672452a3b66dd12d85b47d8e86ef3bc
diegofdg <diegofdg@gmail.com>
2022-03-18 19:09:39 -0300

    terminado mensaje enviado en formulario de contacto

--- crypto/src/app/layout/contact/contact.component.ts
+++ crypto/src/app/layout/contact/contact.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-//import { format } from 'path';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
+
 export class ContactComponent implements OnInit {
 contactForm: FormGroup;
 spinner:boolean = false;
-mensaje:boolean = false;
+mensajeEnviado:boolean = false;
 
   constructor(private fb: FormBuilder) {
   	this.contactForm = this.fb.group({
@@ -22,58 +21,24 @@ mensaje:boolean = false;
   }
 
   ngOnInit(): void { }
-
+   
   contact() {
     const nombre = this.contactForm.get('nombre')?.value;
     const email = this.contactForm.get('email')?.value;
     const mensaje = this.contactForm.get('mensaje')?.value;
+    const datosAEnviar = { nombre, email, mensaje };
 
-    console.log('Pasó la validación');
-    console.log('nombre: ', nombre);
-    console.log('email: ', email);
-    console.log('mensaje: ', mensaje);
+    console.log(datosAEnviar);
+
     this.spinner = true;
 
     setTimeout(()=>{
       this.spinner = false;
-      this.mensaje = true;
-      
-      
-      
+      this.mensajeEnviado = true;
 
-    },3000)
-
-    
-
-    setTimeout(()=>{
-      this.mensaje = false;
-      
-      
-
-    },3000);
-
-    
-
-
-    
-
-    
-    
+      setTimeout(()=>{
+        this.mensajeEnviado = false;
+      },4000);
+    },1500);
   }
-
-  
-
-  /* mostrarSpinner() {
-    
-
-    const spinner = document.createElement('div');
-    spinner.classList.add('spinner');
-
-    spinner.innerHTML = `        
-        <div class="bounce1"></div>
-        <div class="bounce2"></div>
-        <div class="bounce3"></div>        
-    `;
-    resultado.appendChild(spinner);
-} */
 }
